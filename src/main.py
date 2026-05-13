4dc3546cd38393d66c9a88cbeb8aab58408d8992
kang <dev.kangcho@gmail.com>
2026-05-13 22:31:17 +0900

    hough

--- src/main.py
+++ src/main.py
@@ -8,16 +8,27 @@ import Viewers
 import ImageProcessing as ip
 
 if __name__ == "__main__":
-    img = ImageUtils.readImage(ImageUtils.getDataPathWithFile("cat.png"))
-    imgThresholdBinary = ip.thresholding(img, ip.ThresholdType.BINARY)
-    imgThresholdBinaryInv = ip.thresholding(img, ip.ThresholdType.BINARY_INV)
-    imgThresholdTrunc = ip.thresholding(img, ip.ThresholdType.TRUNC)
-    imgThresholdToZero = ip.thresholding(img, ip.ThresholdType.TOZERO)
-    imgThresholdToZeroInv = ip.thresholding(img, ip.ThresholdType.TOZERO_INV)
-    imgThresholdAdaptiveMean = ip.thresholding(img, ip.ThresholdType.ADAPTIVE_MEAN, block_size=11, c=2)
-    imgThresholdAdaptiveGaussian = ip.thresholding(img, ip.ThresholdType.ADAPTIVE_GAUSSIAN, block_size=11, c=2)
-    imgThresholdOtsu = ip.thresholding(img, ip.ThresholdType.OTSU)
+    img = ImageUtils.readImage(ImageUtils.getDataPathWithFile("road.png"))
+    imgGauss = ip.convolution3x3(img, ip.KernelType.GAUSSIAN_BLUR)
+    lines = ip.HoughLines(imgGauss, 1, np.pi/180, 200)
 
-    viewer = view.MultiImageViewer.from_images(img, imgThresholdBinary, imgThresholdBinaryInv, imgThresholdTrunc, imgThresholdToZero, imgThresholdToZeroInv, imgThresholdAdaptiveMean, imgThresholdAdaptiveGaussian, imgThresholdOtsu, sync_view=False)
+    if lines is not None:
+        for line in lines:
+            rho, theta = line[0]
+            a = np.cos(theta)
+            b = np.sin(theta)
+            x0 = a * rho
+            y0 = b * rho
+            x1 = int(x0 + 1000 * (-b))
+            y1 = int(y0 + 1000 * (a))
+            x2 = int(x0 - 1000 * (-b))
+            y2 = int(y0 - 1000 * (a))
+            slope = (y2 - y1) / (x2 - x1)
+            
+            if abs(slope) < 0.3:
+                continue
+            
+            cv.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+    viewer = view.MultiImageViewer.from_images(img, sync_view=False)
     viewer.run()
-
